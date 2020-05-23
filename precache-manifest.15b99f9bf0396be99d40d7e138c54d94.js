0251acb43690ec641e7cd0897e10510a65412b6d
Abhishek Pandya <abhishek.pandya@tothenew.com>
2020-05-23 20:37:52 +0530

    Updates

---
+++ precache-manifest.15b99f9bf0396be99d40d7e138c54d94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bc956bbf62c258ec9ff18a2a571290",
+    "url": "/Image-Search-Gallery/index.html"
+  },
+  {
+    "revision": "31a02eb7c4c3b729f970",
+    "url": "/Image-Search-Gallery/static/css/main.1f57853e.chunk.css"
+  },
+  {
+    "revision": "e2b9e20129727b16cedb",
+    "url": "/Image-Search-Gallery/static/js/2.47e3ff6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Image-Search-Gallery/static/js/2.47e3ff6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a02eb7c4c3b729f970",
+    "url": "/Image-Search-Gallery/static/js/main.774be1b7.chunk.js"
+  },
+  {
+    "revision": "a8b681148a391683b74a",
+    "url": "/Image-Search-Gallery/static/js/runtime-main.5f01d427.js"
+  }
+]);
